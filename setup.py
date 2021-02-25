7f5cb330116a032d57126b5e2b24b6df211024a1
Andrew Ott <64142335+Ottpocket@users.noreply.github.com>
2021-02-25 12:41:23 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Lightweight_check_stonk',
+    version='0.0.01',
+    description='light_weight functions from yahoo_fin',
+    author='Ottpocket',
+    url='https://github.com/Ottpocket/Lightweight_check_stonk',
+    packages=find_packages(),
+    install_requires=["requests"]
+)
