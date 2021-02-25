a87114988f8560981f9f221598ed8928d4d390f9
Andrew Ott <64142335+Ottpocket@users.noreply.github.com>
2021-02-25 12:42:43 -0600

    Update check_stonk.py
    Added the reduce memory to the check_stonk.py

--- Lightweight_check_stonk/check_stonk.py
+++ Lightweight_check_stonk/check_stonk.py
@@ -2,7 +2,43 @@ import pandas as pd
 import requests
 import json
 import datetime
+import numpy as np
 
+def reduce_mem_usage(df):
+    """ iterate through all the columns of a dataframe and modify the data type
+        to reduce memory usage.        
+    """
+    start_mem = df.memory_usage().sum() / 1024**2
+    
+    for col in df.columns:
+        col_type = df[col].dtype
+        
+        #Stop if col of type Datetime
+        if 'ate' in col_type.name:
+            pass
+        elif col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+        else:
+            del df[col] 
+    
+    return df
 
 
 def build_url(ticker, start_date = None, end_date = None, interval = "1d"):
